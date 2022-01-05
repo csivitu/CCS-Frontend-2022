fe616ccf9578e1a512136fb25d6c824811287920
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2022-01-05 23:02:32 +0400

    feat: routes

---
+++ pages/login.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Login = () => {
+    return(
+        <>
+        <Head>
+            <title>CCS | Login</title>
+            <meta name="keywords" content="ccs" />
+        </Head>
+        <div>
+            <h1> Login here</h1>
+        </div>
+        </>
+    );
+}
+
+export default Login;
---
+++ pages/signup.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Signup = () => {
+    return(
+        <>
+        <Head>
+            <title>CCS | Signup</title>
+            <meta name="keywords" content="ccs" />
+        </Head>
+        <div>
+            <h1> Sign Up here</h1>
+        </div>
+        </>
+    );
+}
+
+export default Signup;
