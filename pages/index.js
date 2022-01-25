7e76edde1d9471d5442ed7579c83482db4016a62
nimishjn <nimishjain100701@gmail.com>
2022-01-26 01:57:17 +0530

    feat: Added tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,12 +11,16 @@ export default function Home() {
       </Head>
       <div>
         <h1 className={styles.title}> HomePage</h1>
-        <p className={styles.text}> Hello there, CSI-VIT welcomes you to CCS! </p>
+        <p className={styles.text}>
+          Hello there, CSI-VIT welcomes you to CCS!{" "}
+        </p>
         <Link href="/login">
-        <a className={styles.btn}>Login</a></Link>
+          <a className={styles.btn}>Login</a>
+        </Link>
         <Link href="/signup">
-        <a className={styles.btn}>Sign Up</a></Link>
+          <a className={styles.btn}>Sign Up</a>
+        </Link>
       </div>
     </>
-  )
+  );
 }
