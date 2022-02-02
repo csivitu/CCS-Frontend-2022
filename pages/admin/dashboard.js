77615c9f8affb5957311f6746112dc6f575899fc
salt57 <sourishgupta02@gmail.com>
2022-02-02 17:45:59 +0530

    feat: admin page

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,5 +1,12 @@
+import { adminGetRequest } from "../../lib/axios";
 
 const dashboard = () => {
+  async function getUsers() {
+    const data = await adminGetRequest();
+    console.log(data);
+  }
+
+  getUsers()
   return <div>Hi soureesh</div>;
 };
 
