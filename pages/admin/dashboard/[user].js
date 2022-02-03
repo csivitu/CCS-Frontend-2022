266b4121f7ce7fa565a185009fa6288ba01394e4
salt57 <sourishgupta02@gmail.com>
2022-02-03 19:43:42 +0530

    feat: completed admin page

--- pages/admin/dashboard/[user].js
+++ pages/admin/dashboard/[user].js
@@ -1,13 +1,70 @@
 import { Button } from "@mui/material";
-import nookies from "nookies";
+import nookies, { parseCookies } from "nookies";
+import { useState } from "react";
 import { CustomInput } from "../../../components/CustomForm";
 import Questions from "../../../components/Questions";
-import { adminGetUserRequest } from "../../../lib/axios";
+import { adminChange, adminGetUserRequest } from "../../../lib/axios";
 
 const User = ({ result: { user, questions } }) => {
-  const submitHandler = async() => {
+  const [techMarks, setTechMarks] = useState(user.marks ? user.marks.tech : 0);
+  const [managementMarks, setManagementMarks] = useState(
+    user.marks ? user.marks.management : 0
+  );
+  const [designMarks, setDesignMarks] = useState(
+    user.marks ? user.marks.design : 0
+  );
+  const [videoMarks, setVideoMarks] = useState(
+    user.marks ? user.marks.video : 0
+  );
+  const [techComment, setTechComment] = useState("");
+  const [managementComment, setManagementComment] = useState("");
+  const [designComment, setDesignComment] = useState("");
+  const [videoComment, setVideoComment] = useState("");
 
-  }
+  const submitHandler = async (domain, e) => {
+    e.preventDefault();
+    const cookies = parseCookies();
+    let data;
+    switch (domain) {
+      case "tech":
+        data = {
+          username: user.userId.username,
+          domain,
+          comment: techComment,
+          mark: parseInt(techMarks),
+        };
+        break;
+      case "management":
+        data = {
+          username: user.userId.username,
+          domain,
+          comment: managementComment,
+          mark: parseInt(managementMarks),
+        };
+        break;
+      case "design":
+        data = {
+          username: user.userId.username,
+          domain,
+          comment: designComment,
+          mark: parseInt(designMarks),
+        };
+        break;
+      case "video":
+        data = {
+          username: user.userId.username,
+          domain,
+          comment: videoComment,
+          mark: parseInt(videoMarks),
+        };
+        break;
+      default:
+        break;
+    }
+
+    const res = await adminChange(data, cookies);
+    console.log(res);
+  };
   return (
     <div className="flex flex-col justify-center items-center m-6">
       <div>
@@ -60,35 +117,100 @@ const User = ({ result: { user, questions } }) => {
         );
       })}
       <p className="text-4xl my-4">TECH</p>
-      <form onSubmit={submitHandler} className="flex flex-col gap-2">
-        <label className="text-xl mx-3">Marks</label>
-        <input className="text-slate-900 p-2 px-4 rounded-md" type="number"></input>
-        <label className="text-xl mx-3 mt-5">comments</label>
-        <textarea className="text-slate-900 p-2 px-4 rounded-md"></textarea>
+
+      <p className="my-4">Comments: </p>
+      {user.comments.tech.map((comment, i) => (
+        <p key={i}>{comment}</p>
+      ))}
+
+      <form
+        onSubmit={(e) => submitHandler("tech", e)}
+        className="flex flex-col gap-2 mt-5"
+      >
+        <CustomInput
+          type="number"
+          value={techMarks}
+          setValue={setTechMarks}
+          label="Marks"
+        />
+        <CustomInput
+          type="text"
+          value={techComment}
+          setValue={setTechComment}
+          label="Comments"
+        />
         <Button type="submit">submit</Button>
       </form>
       <p className="text-4xl my-4">MANAGEMENT</p>
-      <form onSubmit={submitHandler} className="flex flex-col gap-2">
-        <label className="text-xl mx-3">Marks</label>
-        <input className="text-slate-900 p-2 px-4 rounded-md" type="number"></input>
-        <label className="text-xl mx-3 mt-5">comments</label>
-        <textarea className="text-slate-900 p-2 px-4 rounded-md"></textarea>
+
+      <p className="my-4">Comments: </p>
+      {user.comments.management.map((comment, i) => (
+        <p key={i}>{comment}</p>
+      ))}
+      <form
+        onSubmit={(e) => submitHandler("management", e)}
+        className="flex flex-col gap-2"
+      >
+        <CustomInput
+          type="number"
+          value={managementMarks}
+          setValue={setManagementMarks}
+          label="Marks"
+        />
+        <CustomInput
+          type="text"
+          value={managementComment}
+          setValue={setManagementComment}
+          label="Comments"
+        />
         <Button type="submit">submit</Button>
       </form>
       <p className="text-4xl my-4">DESIGN</p>
-      <form onSubmit={submitHandler} className="flex flex-col gap-2">
-        <label className="text-xl mx-3">Marks</label>
-        <input className="text-slate-900 p-2 px-4 rounded-md" type="number"></input>
-        <label className="text-xl mx-3 mt-5">comments</label>
-        <textarea className="text-slate-900 p-2 px-4 rounded-md"></textarea>
+
+      <p className="my-4">Comments: </p>
+      {user.comments.design.map((comment, i) => (
+        <p key={i}>{comment}</p>
+      ))}
+      <form
+        onSubmit={(e) => submitHandler("design", e)}
+        className="flex flex-col gap-2"
+      >
+        <CustomInput
+          type="number"
+          value={designMarks}
+          setValue={setDesignMarks}
+          label="Marks"
+        />
+        <CustomInput
+          type="text"
+          value={designComment}
+          setValue={setDesignComment}
+          label="Comments"
+        />
         <Button type="submit">submit</Button>
       </form>
       <p className="text-4xl my-4">VIDEO</p>
-      <form onSubmit={submitHandler} className="flex flex-col gap-2">
-        <label className="text-xl mx-3">Marks</label>
-        <input className="text-slate-900 p-2 px-4 rounded-md" type="number"></input>
-        <label className="text-xl mx-3 mt-5">comments</label>
-        <textarea className="text-slate-900 p-2 px-4 rounded-md"></textarea>
+
+      <p className="my-4">Comments: </p>
+      {user.comments.video.map((comment, i) => (
+        <p key={i}>{comment}</p>
+      ))}
+      <form
+        onSubmit={(e) => submitHandler("video", e)}
+        className="flex flex-col gap-2"
+      >
+        <CustomInput
+          type="number"
+          value={videoMarks}
+          setValue={setVideoMarks}
+          label="Marks"
+        />
+        <CustomInput
+          type="text"
+          value={videoComment}
+          setValue={setVideoComment}
+          label="Comments"
+        />
         <Button type="submit">submit</Button>
       </form>
     </div>
